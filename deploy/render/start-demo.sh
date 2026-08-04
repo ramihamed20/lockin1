d458af601bcd1fd49f248481d37d61fd8be06748
Dentistry <yqkfd9482c@privaterelay.appleid.com>
2026-08-04 04:38:25 +0200

    fix(demo): bind Render port before seeding

--- deploy/render/start-demo.sh
+++ deploy/render/start-demo.sh
@@ -3,6 +3,9 @@ set -eu
 
 : "${PORT:=10000}"
 
+envsubst '${PORT}' < /etc/nginx/templates/default.conf.template > /etc/nginx/conf.d/default.conf
+nginx
+
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
 
@@ -10,6 +13,4 @@ if [ "${LOCKIN_DEMO_SEED:-true}" = "true" ]; then
   python manage.py seed_demo
 fi
 
-envsubst '${PORT}' < /etc/nginx/templates/default.conf.template > /etc/nginx/conf.d/default.conf
-nginx
 exec gunicorn --config config/gunicorn.py config.wsgi:application
